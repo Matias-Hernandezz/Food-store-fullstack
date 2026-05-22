80f59a54fcb4555720095c419735901b86f0519b
GordoMatti <matinicoher@gmail.com>
2026-05-21 22:36:12 -0300

    uow + repository

--- BACKEND/app/modules/dominio_2/Categoria/unit_of_work.py
+++ BACKEND/app/modules/dominio_2/Categoria/unit_of_work.py
@@ -1,6 +1,6 @@
 from sqlmodel import Session
 from app.core.unit_of_work import UnitOfWork # Asumiendo tu import base
-from app.modules.Categoria.repository import CategoriaRepository
+from app.modules.dominio_2.Categoria.repository import CategoriaRepository
 
 class CategoriaUnitOfWork(UnitOfWork):
    
@@ -8,4 +8,4 @@ class CategoriaUnitOfWork(UnitOfWork):
     def __init__(self, session: Session) -> None:
         super().__init__(session)
         self.categorias = CategoriaRepository(session)
-        
+ 
